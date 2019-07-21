26e23b15d7e54013ecd2f13ee9f0f3c51ebadefc
Luis Francisco Angulo Andrade <luisf117@gmail.com>
2019-07-20 20:14:20 -0700

    phonebook steps 4, 5 and 6

--- src/index.js
+++ src/index.js
@@ -3,58 +3,100 @@ const express = require("express");
 const parser = require("body-parser");
 const PORT = 3001;
 
-let persons = contactFile.persons  // Make in-memory copy of persons array
+let persons = contactFile.persons; // Make in-memory copy of persons array
 
 const app = express();
 app.use(parser.json());
 
 // parent
-app.get('/', (req, res) => {
-    res.send('<h1>Parent request</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Parent request</h1>");
+});
 
 // info
-app.get('/info', (req, res) => {
-    const requestTime = new Date()
-    const html = `<p>Phonebook has info for ${persons.length} people</p> <p>${requestTime}</p>`
-    res.send(html)
-})
+app.get("/info", (req, res) => {
+  const requestTime = new Date();
+  const html = `<p>Phonebook has info for ${
+    persons.length
+  } people</p> <p>${requestTime}</p>`;
+  res.send(html);
+});
 
 // all persons
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
-})
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
 
 // specific person
-app.get('/api/persons/:id', (req, res) => {    
-    // Get the person id from the params of the request
-    const personId = Number(req.params.id)
-    const person = persons.find(p => p.id === personId)
-    // Check if we have a valid person
-    if (person) {
-        // Return the serialized person
-        res.json(person)
-    } else {
-        // Not found, send error
-        res.status(404).send()
+app.get("/api/persons/:id", (req, res) => {
+  // Get the person id from the params of the request
+  const personId = Number(req.params.id);
+  const person = persons.find(p => p.id === personId);
+  // Check if we have a valid person
+  if (person) {
+    // Return the serialized person
+    res.json(person);
+  } else {
+    // Not found, send error
+    res.status(404).send();
+  }
+});
+
+/**
+ * Returns null for good requests or an error obj for bad ones
+ * @param {*} newPerson 
+ * @param {*} personsList 
+ */
+const validateNewPerson = (newPerson, personsList) => {
+  try {
+    if (!newPerson.name.length) {
+      return { error: "name must not be empty" };
     }
-})
+    if (!newPerson.number.length) {
+      return { error: "number must not be empty" };
+    }
+    const repeat = personsList.find(p => p.name === newPerson.name);
+    if (repeat) {
+      return { error: "name must be unique" };
+    }
+  } catch (err) {
+    console.log(err);
+    return { error: "missing attribute" };
+  }
+};
 
 // new contact
-app.post('/api/persons', (req, res) => {
-    
-})
+app.post("/api/persons", (req, res) => {
+  const newPerson = req.body;
+  // if the body's not empty, save the person, else return an error
+  let error = validateNewPerson(newPerson, persons);
+  if (!error) {
+    newPerson.id = Math.floor(Math.random() * 10000);
+    persons = persons.concat(newPerson);
+    res.status(202).send();
+  } else {
+    res.status(400).json(error);
+  }
+});
 
 // update contact
-app.put('/api/persons/:id', (req, res) => {
-    
-})
+app.put("/api/persons/:id", (req, res) => {});
 
 // delete contact
-app.delete('/api/persons/:id', (req, res) => {
-    const personId = Number(req.params.id)
-})
+app.delete("/api/persons/:id", (req, res) => {
+  // Get the person id from the params of the request
+  const personId = Number(req.params.id);
+  const prevLen = persons.length;
+  persons = persons.filter(p => p.id !== personId);
+  // If the list is shorter, we succeeded
+  if (persons.length !== prevLen) {
+    res.status(200).send();
+  } else {
+    // Not found, send error
+    res.status(404).send();
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`App listening on port: ${PORT}`)
+  console.log(`App listening on port: ${PORT}`);
 });
