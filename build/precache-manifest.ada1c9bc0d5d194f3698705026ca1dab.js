4008f89c65b4298f6c3c5e12c843d52c403c2ce6
Luis Francisco Angulo Andrade <luisf117@gmail.com>
2019-07-24 02:11:24 -0700

    production build, patched build script

---
+++ build/precache-manifest.ada1c9bc0d5d194f3698705026ca1dab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1a0d83f50e2b0006be4d2624b00444",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3838c138d484160659a",
+    "url": "/static/css/main.f7d643e7.chunk.css"
+  },
+  {
+    "revision": "d538652694bd439d49af",
+    "url": "/static/js/2.f2f882f0.chunk.js"
+  },
+  {
+    "revision": "e3838c138d484160659a",
+    "url": "/static/js/main.9bbb2f5d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
