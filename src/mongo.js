ba4fa8515ae075c0a117e831967edd48de9749f6
Luis Francisco Angulo Andrade <luisf117@gmail.com>
2019-07-26 20:08:41 -0700

    fos Ex3.12 comments and code cleanup

--- src/mongo.js
+++ src/mongo.js
@@ -1,5 +1,7 @@
+// We use this library for interacting with the mongodb atlas service
 const mongoose = require("mongoose");
 
+// Check the CLI arguments (passed in the process.argv array)
 const password = process.argv[2];
 const name = process.argv[3];
 const number = process.argv[4];
@@ -8,12 +10,17 @@ if (!password) {
   process.exit(1);
 }
 
+// Atlas will create a new DB if the name does not match an existing one
 const dbName = "phonebook-app";
 const dbURI = `mongodb+srv://fosapps:${password}@luisangulo-ucxal.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-mongoose.connect(dbURI, { useNewUrlParser: true }).then((res) => res);
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
+
+// DB ops are async, so the example code from fos part3c won't work.
+// The code in the "once" callback will run when the connection opens
+mongoose.connect(dbURI, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+// TODO - refactor : This chunk of code is NOT good. Give it good design once I learn how it should look
+db.once("open", () => {
   const contactSchema = new mongoose.Schema({ name: String, number: String });
   const Contact = mongoose.model("Contact", contactSchema);
   if (name && number) {
@@ -21,18 +28,10 @@ db.once('open', () => {
     contact
       .save()
       .then(res => console.log(`Added ${name}, number ${number} to phonebook`))
-      .then(() => {
-        console.log("Create contact close");
-        mongoose.connection.close();
-      });
+      .then(() => mongoose.connection.close());
   } else {
     Contact.find({})
-      .then(result => {
-        result.forEach(note => console.log(note));
-      })
-      .then(() => {
-        console.log("Find contact close");
-        mongoose.connection.close();
-      });
+      .then(result => result.forEach(note => console.log(note)))
+      .then(() => mongoose.connection.close());
   }
 });
